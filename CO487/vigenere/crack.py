131d4b1ce67fdf2391962ef10a316f448f59019b
Andrei Savin <glork2006@gmail.com>
2016-01-07 23:28:56 -0500

    Sketch

---
+++ CO487/vigenere/crack.py
@@ -0,0 +1,14 @@
+import fileinput
+from config import *
+
+# Read input and remove spaces/line breaks
+input = ''
+for line in fileinput.input():
+    input = input + line.replace(" ", "").strip()
+
+# 1) Find Key Length
+
+# 2) Find Key
+
+# 3) Guess encrypted text
+
