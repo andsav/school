cacbad16b33e002c862c3f3ede01de8678c7a9fe
Andrei Savin <glork2006@gmail.com>
2016-01-24 18:59:15 +0000

    Count Inversions algorithm

--- CS341/CountInversions/main.cc
+++ CS341/CountInversions/main.cc
@@ -4,13 +4,14 @@
 
 using namespace std;
 
-int mergeSort(vector<int>& integers, int left, int right);
+unsigned int merge(vector<int> &integers, vector<int> &left, vector<int> &right);
+unsigned int merge_and_count(vector<int> &integers);
 
 int main() {
     stringstream ss;
     string s;
-    vector<int> integers;
     int i;
+    vector<int> integers;
 
     while(cin >> s) {
         ss.clear();
@@ -19,15 +20,46 @@ int main() {
         integers.push_back(i);
     }
 
-    cout << mergeSort(integers, 1, 1) << endl;
-
-    for(i = 0; i < integers.size(); ++i) {
-        cout << integers[i] << endl;
-    }
-
-	return 0;
+    cout << merge_and_count(integers) << endl;
+	
+    return 0;
 }
 
-int mergeSort(vector<int>& integers, int left, int right) {
-    return 1;
+unsigned int merge(vector<int> &integers, vector<int> &left, vector<int> &right) {
+    unsigned int i, j, count;
+    i = 0;
+    j = 0;
+    count = 0;
+
+    while(i < left.size() or j < right.size()) {
+        if(i == left.size()) {
+            integers[i+j] = right[j];
+            j++;
+        }
+        else if(j == right.size()) {
+            integers[i+j] = left[i];
+            i++;
+        }
+        else if(left[i] <= right[j]) {
+            integers[i+j] = left[i];
+            i++;
+        }
+        else {
+            integers[i+j] = right[j];
+            count += left.size()- i;
+            j++;
+        }
+    }
+
+    return count;
+}
+
+unsigned int merge_and_count(vector<int> &integers) {
+    if(integers.size() < 2) return 0;
+
+    int middle = (integers.size() + 1)/2;
+    vector<int> left(integers.begin(), integers.begin() + middle);
+    vector<int> right(integers.begin() + middle, integers.end());
+
+    return merge_and_count(left) + merge_and_count(right) + merge(integers, left, right);
 }
