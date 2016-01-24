2fb9d7e50df0017a38132bf327bead050c7eabc1
Andrei Savin (MacBook Root) <glork2006@gmail.com>
2016-01-24 16:49:21 -0500

    CS341 start

--- CS341/CountInversions/main.cc
+++ CS341/CountInversions/main.cc
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-int main() {	
-	cout << "HELLO WORLD" << endl;
+int mergeSort(vector<int>& integers, int left, int right);
+
+int main() {
+    stringstream ss;
+    string s;
+    vector<int> integers;
+    int i;
+
+    while(cin >> s) {
+        ss.clear();
+        ss.str(s);
+        ss >> i;
+        integers.push_back(i);
+    }
+
+    cout << mergeSort(integers, 1, 1) << endl;
+
+    for(i = 0; i < integers.size(); ++i) {
+        cout << integers[i] << endl;
+    }
 
 	return 0;
+}
+
+int mergeSort(vector<int>& integers, int left, int right) {
+    return 1;
 }
