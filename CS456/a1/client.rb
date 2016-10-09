923f76538ba316d67d64bb86e9fcf869cbf609ab
Andrei Savin (MacBook) <glork2006@gmail.com>
2016-10-09 18:37:05 -0400

    CS456 a1 comments

--- CS456/a1/client.rb
+++ CS456/a1/client.rb
@@ -1,38 +1,40 @@
 require 'socket'
 
+# Validate parameters
 raise RuntimeError, 'Invalid argument count' if ARGV.length != 4
+raise RuntimeError, 'Parameter <n_port> must be a number' if not ARGV[1] =~ /^[0-9]+$/
+raise RuntimeError, 'Parameter <req_code> must be a number' if not ARGV[2] =~ /^[0-9]+$/
 
+# Constants
 SERVER_ADDRESS, N_PORT, REQ_CODE, MESSAGE = ARGV
 HOSTNAME = Socket.gethostname.split('.')[0]
 
-server = TCPSocket.open(SERVER_ADDRESS, N_PORT)
+# Open an UDP socket and bind to a free port (for server replies)
 udp_server = UDPSocket.new
 udp_server.bind '', 0
-
 UDP_PORT = udp_server.addr[1]
 
-puts "HOSTNAME=#{HOSTNAME}\n\n"
-
+# Stage 1: Negotiation on TCP channel
+server = TCPSocket.open(SERVER_ADDRESS, N_PORT)
 puts "Server #{server} - TCP connection opened"
 
 server.puts REQ_CODE
 
 server_ret = server.gets
-
 if server_ret.nil?
   server.close
   raise RuntimeError, 'Invalid <req_code>; Server closed connection'
 end
 
 SERVER_UDP_PORT = server_ret.strip
-
 puts "Server #{server} - Returned UDP port: #{SERVER_UDP_PORT}"
 puts "Server #{server} - Closing TCP connection\n\n"
-
 server.close
 
+# Stage 2: UDP sockets communication
 udp_server.send MESSAGE, 0, SERVER_ADDRESS, SERVER_UDP_PORT
 udp_server.send UDP_PORT.to_s, 0, SERVER_ADDRESS, SERVER_UDP_PORT
 udp_server.send HOSTNAME, 0, SERVER_ADDRESS, SERVER_UDP_PORT
 
+# Returned reverse message
 puts udp_server.recv(1024)
--- CS456/a1/server.rb
+++ CS456/a1/server.rb
@@ -1,27 +1,29 @@
 require 'socket'
 
+# Validate parameters
 raise RuntimeError, 'No <req_code> specified' if ARGV.length < 1
+raise RuntimeError, 'Parameter <req_code> must be a number' if not ARGV[0] =~ /^[0-9]+$/
 
+# Start TCP server at a free port
 server = TCPServer.open(0)
-udp_server = UDPSocket.new
 
-udp_server.bind '', 0
-
-UDP_PORT = udp_server.addr[1]
+# Constants
 N_PORT = server.addr[1]
 REQ_CODE = ARGV[0].strip
 HOSTNAME = Socket.gethostname.split('.')[0]
 
 puts "SERVER_PORT=#{N_PORT}"
-puts "REQ_CODE=#{REQ_CODE}"
-puts "UDP_PORT=#{UDP_PORT}\n\n"
+puts "REQ_CODE=#{REQ_CODE}\n\n"
 
+# Abort threads if exception is caught
 Thread.abort_on_exception = true
 
+# Server loop
 loop do
   Thread.start(server.accept) do |client|
     puts "Client #{client} - TCP connection opened"
 
+    # Get the <req_code> supplied by client and verify it is valid
     req_code = client.gets.strip
 
     if req_code != REQ_CODE
@@ -30,23 +32,36 @@ loop do
       client.close
     else
       puts "Client #{client} - <req_code> accepted!"
-      client.puts UDP_PORT
 
+      # Open an UDP socket and bind to a free port
+      udp_server = UDPSocket.new
+      udp_server.bind '', 0
+      udp_port = udp_server.addr[1]
+
+      puts "Server - Listening on UDP port #{udp_port}"
+
+      # Send the UDP port to client
+      client.puts udp_port
+
+      # Receive message and callback instructions from client
       message = udp_server.recv(1024)
       client_udp_port = udp_server.recv(10)
       client_hostname = udp_server.recv(512)
       client_hostname = '127.0.0.1' if client_hostname == HOSTNAME
-
       puts "Client #{client} - Received <message>: #{message}"
       puts "Client #{client} - Received callback port: #{client_udp_port}"
       puts "Client #{client} - Received callback hostname: #{client_hostname}"
 
+      # Reply back to client
       udp_server.send message.reverse, 0, client_hostname, client_udp_port
+      puts 'Server - Replied with reversed message'
 
-      puts "Client #{client} - Returned reversed message"
+      # Close UDP socket and TCP connection with client
+      udp_server.close
+      puts 'Server - Closed UDP connection'
+      client.close
       puts "Client #{client} - Terminated\n\n"
 
-      client.close
     end
 
   end
