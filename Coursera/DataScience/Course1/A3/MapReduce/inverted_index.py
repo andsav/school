d0604f65791913dbf3a00a7cab31314d67553f19
Andrei Savin (MacBook) <glork2006@gmail.com>
2016-11-12 18:28:40 -0500

    DatScience a3

---
+++ Coursera/DataScience/Course1/A3/MapReduce/inverted_index.py
@@ -0,0 +1,16 @@
+import MapReduce
+import sys
+
+mr = MapReduce.MapReduce()
+
+def mapper(record):
+    for word in record[1].split():
+        mr.emit_intermediate(word, record[0])
+
+def reducer(key, list_of_values):
+    mr.emit((key, list(set(list_of_values))))
+
+
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
---
+++ Coursera/DataScience/Course1/README.md
@@ -0,0 +1,4 @@
+Data Manipulation: Systems and Algorithms
+=============================
+
+https://www.coursera.org/learn/data-manipulation
