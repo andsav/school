c33c4384b7a026170315afecaef3a825df90cdb0
Andrei Savin (MacBook) <andrei@andreisavin.com>
2016-11-27 18:34:09 -0500

    CS456 A3

--- CS456/A3/Router/packet.rs
+++ CS456/A3/Router/packet.rs
@@ -1,16 +1,42 @@
+pub trait Encode {
+    type X;
+    fn encode(&self) -> Self::X;
+}
+
 pub struct Hello {
-    router_id: u8,
-    link_id: u8
+    pub router_id: u8,
+    pub link_id: u8
+}
+
+impl Encode for Hello {
+    type X = [u8; 2];
+    fn encode(&self) -> Self::X {
+        [self.router_id, self.link_id]
+    }
 }
 
 pub struct PSU {
-    sender: u8,
-    router_id: u8,
-    link_id: u8,
-    cost: u8,
-    via: u8
+    pub sender: u8,
+    pub router_id: u8,
+    pub link_id: u8,
+    pub cost: u8,
+    pub via: u8
+}
+
+impl Encode for PSU {
+    type X = [u8; 5];
+    fn encode(&self) -> Self::X  {
+        [self.sender, self.router_id, self.link_id, self.cost, self.via]
+    }
 }
 
 pub struct Init {
-    router_id: u8
+    pub router_id: u8
+}
+
+impl Encode for Init {
+    type X = [u8; 1];
+    fn encode(&self) -> Self::X  {
+        [self.router_id]
+    }
 }
--- CS456/A3/Router/router.rs
+++ CS456/A3/Router/router.rs
@@ -2,8 +2,9 @@ mod helpers;
 mod packet;
 mod state;
 
-use std::{env, net};
+use std::{env, net, thread};
 use std::fs::File;
+use packet::Encode;
 
 fn main() {
     /*
@@ -13,10 +14,11 @@ fn main() {
     let args: Vec<_> = env::args().collect();
 
     if args.len() != 5 {
-        helpers::abort("Expected 4 arguments: <router_id>, <nse_hostname>, <router_port>, <nse_port>");
+        helpers::abort("Expects arguments: <router_id>, <nse_hostname>, <router_port>, <nse_port>");
     }
 
     let router_id: u8 = args[1].parse::<u8>().expect("Invalid router_id");
+    let s_router_id: &str = &*args[1].to_string();
     let nse_hostname: &str = &*args[2].to_string();
     let router_port: u16 = args[3].parse::<u16>().expect("Invalid router_port");
     let nse_port: u16 = args[4].parse::<u16>().expect("Invalid nse_port");
@@ -24,18 +26,25 @@ fn main() {
     /*
     Initialization
     */
-    let socket = net::UdpSocket::bind((nse_hostname, nse_port)).expect("Error binding to socket");
+    let socket = net::UdpSocket::bind(("localhost", router_port)).expect("Error binding to socket");
 
-    let log_file_name = "router".to_string() + &*args[1] + ".log";
+    let log_file_name = "router".to_string() + s_router_id + ".log";
     let log = File::create(&log_file_name).expect("Unable to create log file");
 
     /*
     Main Program
     */
-    let mut buf = [0; 10];
-    loop {
-        socket.recv_from(&mut buf);
-    }
 
-    println!("{} {} {} {}", router_id, nse_hostname, router_port, nse_port);
+    // 1) Send Init
+
+    let init = packet::Init{router_id: router_id}.encode();
+    socket.send_to(&init, (nse_hostname, nse_port));
+
+
+    // 2) Send Hello to neighbours
+
+
+    // 3) Receive Hello and LSPDU and flood LSPDU
+
+
 }
