ac1d86c392d6a72c4517442fee7c5f201d28b9eb
Andrei Savin (MacBook) <andrei@andreisavin.com>
2016-11-27 19:09:11 -0500

    CS456 A3

--- CS456/A3/Router/packet.rs
+++ CS456/A3/Router/packet.rs
@@ -1,6 +1,7 @@
-pub trait Encode {
+pub trait Packet {
     type X;
     fn encode(&self) -> Self::X;
+    fn log_message(&self) -> String;
 }
 
 pub struct Hello {
@@ -8,10 +9,13 @@ pub struct Hello {
     pub link_id: u8
 }
 
-impl Encode for Hello {
+impl Packet for Hello {
     type X = [u8; 2];
     fn encode(&self) -> Self::X {
         [self.router_id, self.link_id]
+    }
+    fn log_message(&self) -> String {
+        String::from("HELLO link_id: ") + &*self.link_id.to_string()
     }
 }
 
@@ -23,10 +27,13 @@ pub struct PSU {
     pub via: u8
 }
 
-impl Encode for PSU {
+impl Packet for PSU {
     type X = [u8; 5];
     fn encode(&self) -> Self::X  {
         [self.sender, self.router_id, self.link_id, self.cost, self.via]
+    }
+    fn log_message(&self) -> String {
+        String::from("LSPDU link_id: ") + &*self.link_id.to_string()
     }
 }
 
@@ -34,9 +41,12 @@ pub struct Init {
     pub router_id: u8
 }
 
-impl Encode for Init {
+impl Packet for Init {
     type X = [u8; 1];
     fn encode(&self) -> Self::X  {
         [self.router_id]
+    }
+    fn log_message(&self) -> String {
+        String::from("INIT")
     }
 }
