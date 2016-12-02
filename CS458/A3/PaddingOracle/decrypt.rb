5beaf210248b95f8404322ac22de639295b91ad3
Andrei Savin (MacBook) <andrei@andreisavin.com>
2016-12-02 06:34:28 -0500

    CS458 a3 refactoring (need to fix padding)

--- CS458/A3/PaddingOracle/decrypt.rb
+++ CS458/A3/PaddingOracle/decrypt.rb
@@ -3,10 +3,30 @@ load 'oracle.rb'
 
 raise RuntimeError, 'Expected cookie as argument' unless ARGV.length == 1
 
-URL = 'http://ugster21.student.cs.uwaterloo.ca:4555'
+URL = 'http://localhost:4555'
 
 COOKIE = Base64.strict_decode64(ARGV[0])
 blocks = COOKIE.chars.each_slice(16).map(&:join)
 IV = blocks.shift
 
-puts Oracle.decrypt(blocks).join.unpack('C*')
+debug "#{blocks.length} blocks + IV"
+
+puts Oracle.decrypt(blocks).join
+
+
+=begin
+case ARGV.length
+  when 1
+    puts Oracle.decrypt(blocks).join
+  when 2
+    KEY = ARGV[1]
+
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.decrypt
+    cipher.iv = IV
+    cipher.key = KEY
+    cipher.padding = 0
+    puts cipher.update(blocks.join) + cipher.final
+  else raise RuntimeError, 'Expected cookie as argument'
+end
+=end
--- CS458/A3/PaddingOracle/oracle.rb
+++ CS458/A3/PaddingOracle/oracle.rb
@@ -1,6 +1,7 @@
 require 'base64'
 require 'open-uri'
 
+
 BLOCK_SIZE = 16
 LAST_BYTE = BLOCK_SIZE-1
 
@@ -23,6 +24,10 @@ module Oracle
     end
   end
 
+  def random_bytes(size)
+    Array.new(size) { rand(0xff) }
+  end
+
   def xor(a, b)
     [a.unpack('C*'), b.unpack('C*')].transpose.map { |x| x.reduce(:^) }.pack('C*')
   end
@@ -32,7 +37,8 @@ module Oracle
   end
 
   def last_word(block)
-    decoded = Array.new(LAST_BYTE+1) { rand(0xff) }
+    decoded = random_bytes(BLOCK_SIZE)
+    i = 0
 
     (0..0xff).to_a.shuffle.each do |x|
       decoded[-1] = x
@@ -41,38 +47,29 @@ module Oracle
 
     (1..LAST_BYTE).to_a.reverse.each do |n|
       r = decoded
-      r[LAST_BYTE-n] ^= 1
-      unless test_helper(r, block)
-        (LAST_BYTE-n..LAST_BYTE).each do |x|
-          decoded[x] ^= n
-        end
-        return [decoded, LAST_BYTE - n]
-      end
+      r[LAST_BYTE-n+1] ^= 1
+
+      return decoded[LAST_BYTE-n+1..LAST_BYTE].collect { |x| x^n } unless test_helper(r, block)
     end
 
-    decoded[-1] ^= 1
-
-    [decoded, LAST_BYTE]
+    [ decoded[-1] ^ 1 ]
   end
 
   def decrypt_block(block)
-    decoded, last_byte_recovered = last_word(block)
+    decoded = last_word(block)
 
-    debug "Last byte decrypted: #{last_byte_recovered}"
+    debug "Decrypted: #{decoded}"
 
-    (1..last_byte_recovered).to_a.reverse.each do |j|
-      r = Array.new(LAST_BYTE+1) { rand(0xff) }
-      (j..LAST_BYTE).each do |k|
-        r[k] = decoded[k] ^ (LAST_BYTE - j + 2)
-      end
+    until decoded.length == BLOCK_SIZE
+      j = BLOCK_SIZE - decoded.length
+      r = random_bytes(j).concat(decoded.collect { |x| x^(LAST_BYTE-j+2) })
+
       (0..0xff).to_a.shuffle.each do |x|
         r[j-1] = x
-        if test_helper(r, block)
-          debug "Decrypted byte #{j}"
-          break
-        end
+        break if test_helper(r, block)
       end
-      decoded[j-1] = r[j-1] ^ (LAST_BYTE - j + 2)
+
+      decoded.unshift r[j-1]^(LAST_BYTE-j+2)
     end
 
     decoded.pack('C*')
