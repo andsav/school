08751edc6879930b5c26cf05f919a6ac5739faa0
Andrei Savin (MacBook) <andrei@andreisavin.com>
2016-12-03 02:25:17 -0500

    CS458 A3 decrypt working with correct padding

--- CS458/A3/PaddingOracle/oracle.rb
+++ CS458/A3/PaddingOracle/oracle.rb
@@ -37,49 +37,56 @@ module Oracle
   end
 
   def last_word(block)
-    decoded = random_bytes(BLOCK_SIZE)
-    i = 0
+    r = random_bytes(BLOCK_SIZE)
+    decoded = Array.new
 
+    # Find the last byte
     (0..0xff).to_a.shuffle.each do |x|
-      decoded[-1] = x
-      break if test_helper(decoded, block)
+      r[-1] = x
+      break if test_helper(r, block)
     end
 
-    (1..LAST_BYTE).to_a.reverse.each do |n|
-      r = decoded
-      r[LAST_BYTE-n+1] ^= 1
+    r[-1] ^= 1
 
-      return decoded[LAST_BYTE-n+1..LAST_BYTE].collect { |x| x^n } unless test_helper(r, block)
-    end
+    # Add to the decoded array as long as bytes are part of the padding
+    begin
+      decoded.unshift r.pop^1
+      r[-1] ^= 1
+    end until test_helper(r.concat(decoded), block)
 
-    [ decoded[-1] ^ 1 ]
+    decoded[0] ^= decoded.length
+
+    decoded
   end
 
   def decrypt_block(block)
     decoded = last_word(block)
 
-    debug "Decrypted: #{decoded}"
-
     until decoded.length == BLOCK_SIZE
-      j = BLOCK_SIZE - decoded.length
-      r = random_bytes(j).concat(decoded.collect { |x| x^(LAST_BYTE-j+2) })
+
+      debug "Decrypted: #{decoded}"
+
+      r = random_bytes(BLOCK_SIZE - decoded.length).concat(decoded)
 
       (0..0xff).to_a.shuffle.each do |x|
-        r[j-1] = x
+        r[-decoded.length-1] = x
         break if test_helper(r, block)
       end
 
-      decoded.unshift r[j-1]^(LAST_BYTE-j+2)
+      decoded.unshift r[-decoded.length-1] ^ (decoded.length+1)
     end
 
     decoded.pack('C*')
   end
 
   def decrypt(blocks)
-    blocks[0] = xor(IV, decrypt_block(blocks[0]))
+    decrypted = Array.new
+
+    decrypted[0] = xor(IV, decrypt_block(blocks[0]))
     (1..blocks.size-1).each do |b|
-      blocks[b] = xor(blocks[b-1], decrypt_block(blocks[b]))
+      decrypted[b] = xor(blocks[b-1], decrypt_block(blocks[b]))
     end
-    blocks
+
+    decrypted
   end
 end
