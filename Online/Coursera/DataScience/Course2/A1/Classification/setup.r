cc50f05c4c33e235a2a876c0f1b307858a9e7177
Andrei Savin (MacBook) <andrei@andreisavin.com>
2016-12-16 18:16:38 -0500

    Cleanup

--- Online/Coursera/DataScience/Course2/A1/Classification/setup.r
+++
@@ -1,5 +0,0 @@
-install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools'))
-devtools::install_github('IRkernel/IRkernel')
-IRkernel::installspec() 
-IRkernel::installspec(name = 'ir33', displayname = 'R 3.3')
-
